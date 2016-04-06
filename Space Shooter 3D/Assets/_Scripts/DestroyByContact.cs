8cd877778d1437a19e67d59c05fb490d1d12b42f
the-ginsberg <jeffginsberg87@gmail.com>
2016-04-06 16:38:12 -0400

    Added Level Hazards, Collision Detection, and Explosion Animations.

---
+++ Space Shooter 3D/Assets/_Scripts/DestroyByContact.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
